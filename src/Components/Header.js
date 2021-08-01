8c795466495900a72d58a1dceb1f03cce6f1c076
cyberkillua <moshood988@gmail.com>
2021-08-01 01:04:08 +0100

    nav logo done

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="navigation">
+        <p>
+          nigeria<span className="logo">BanksXYZ</span>
+        </p>
+      </nav>
+      <section className="header-primary">
+          <p className="header-primary-text">
+               Search for any Nigerian bank USSD 
+          </p>
+          <div className="header-search">
+              <input type="text" placeholder="Bank name" />
+          </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
