e84ccf8a3a0fe3a54f9102f8ad36c560b0f0d1cf
cyberkillua <moshood988@gmail.com>
2021-08-02 19:15:52 +0100

    banks section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Banks from "./Components/Banks";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banks />
     </div>
   );
 }
