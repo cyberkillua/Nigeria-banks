804b7a92f505f8eb237bedcda0d768b22c959931
cyberkillua <moshood988@gmail.com>
2021-08-02 17:40:10 +0100

    search bar UI for hrader done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,10 +5,14 @@ const Header = () => {
         <p>
           nigeria<span className="logo">BanksXYZ</span>
         </p>
+        <div className="navigation-search">
+            <input type="text" name="search" placeholder="Search for bank"/>
+            <ion-icon name="search-outline" className="icon"></ion-icon>
+        </div>
       </nav>
       <section className="header-primary">
           <p className="header-primary-text">
-               Search for any Nigerian bank USSD 
+              Generate transfer code
           </p>
           <div className="header-primary-search">
               <input type="text" placeholder="Bank name" />
